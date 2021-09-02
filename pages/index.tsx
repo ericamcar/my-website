2abe5eedf461897662337ee590c0e4cdc9ea07e1
Erica Carvalho <ericamcar28@gmail.com>
2021-09-02 01:58:21 +0100

    add home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import type { NextPage } from "next";
+import { FaLaptopCode } from "react-icons/fa";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <h1 className="heading-primary">Home</h1>
-    </>
+    <div className="home">
+      <FaLaptopCode className="logo-image" />
+      <h1 className="heading-primary">Erica Melo de Carvalho</h1>
+      <h2 className="heading-secondary">Software Engineer</h2>
+      <h3 className="my-2">
+        React | Redux | Node | Express | JavaScript | Typescript | SASS | HTML |
+        CSS | MongoDB | PostgreSQL | Git | GitHub | Bash | Ruby | Ruby on Rails
+      </h3>
+    </div>
   );
 };
 
